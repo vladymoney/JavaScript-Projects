b18a1463e53d3d05a9bd642137715adb7e8ad4fd
Vladimir Nankov <102216547+vladymoney@users.noreply.github.com>
2023-07-25 18:53:47 +0300

    Listening For load and hashchange Events

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -31,11 +31,14 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -161,3 +164,7 @@ const showRecipe = async function () {
 };
 
 showRecipe();
+['hashchange', ['load']].forEach(e => window.addEventListener(e, showRecipe));
+
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
